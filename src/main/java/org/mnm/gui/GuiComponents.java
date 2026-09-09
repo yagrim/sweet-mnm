43411b6038584f6936c6c6310a625025564d4368
yagrim <280896461+yagrim@users.noreply.github.com>
2026-09-10 00:23:52 +0200

    Scale checkbox

--- src/main/java/org/mnm/gui/GuiComponents.java
+++ src/main/java/org/mnm/gui/GuiComponents.java
@@ -1,5 +1,6 @@
 package org.mnm.gui;
 
+import javax.swing.Icon;
 import javax.swing.JComponent;
 import javax.swing.UIDefaults;
 import javax.swing.UIManager;
@@ -10,6 +11,8 @@ import javax.swing.plaf.FontUIResource;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.Enumeration;
 
 public class GuiComponents {
@@ -24,9 +27,43 @@ public class GuiComponents {
                 defaults.put(key, new FontUIResource(font.deriveFont(font.getSize2D() * scale)));
             }
         }
+        scaleIcon("CheckBox.icon", scale);
+        scaleIcon("RadioButton.icon", scale);
+    }
+
+    private static void scaleIcon(String key, float scale) {
+        Icon icon = UIManager.getIcon(key);
+        if (icon != null) {
+            UIManager.put(key, new ScaledIcon(icon, scale));
+        }
     }
 
     static void setFontSize(JComponent component, float size) {
         component.setFont(component.getFont().deriveFont(size));
+    }
+
+    private record ScaledIcon(Icon delegate, float scale) implements Icon {
+
+        @Override
+        public int getIconWidth() {
+            return Math.round(delegate.getIconWidth() * scale);
+        }
+
+        @Override
+        public int getIconHeight() {
+            return Math.round(delegate.getIconHeight() * scale);
+        }
+
+        @Override
+        public void paintIcon(Component component, Graphics graphics, int x, int y) {
+            Graphics2D scaledGraphics = (Graphics2D) graphics.create();
+            try {
+                scaledGraphics.translate(x, y);
+                scaledGraphics.scale(scale, scale);
+                delegate.paintIcon(component, scaledGraphics, 0, 0);
+            } finally {
+                scaledGraphics.dispose();
+            }
+        }
     }
 }
