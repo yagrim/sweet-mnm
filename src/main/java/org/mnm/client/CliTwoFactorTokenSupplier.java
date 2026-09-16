9b1474c53699e197c141ca5929c6e969df554824
yagrim <280896461+yagrim@users.noreply.github.com>
2026-09-16 22:30:35 +0200

    WIP: q-n-d CLI TokenProvider, todo test

--- src/main/java/org/mnm/client/CliTwoFactorTokenSupplier.java
+++ src/main/java/org/mnm/client/CliTwoFactorTokenSupplier.java
@@ -3,13 +3,12 @@ package org.mnm.client;
 import java.util.List;
 import java.util.Scanner;
 
-import org.mnm.api.ApiConnector;
-import org.mnm.api.Session;
-import org.mnm.api.TokenSupplier;
-import org.mnm.config.Messages;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.mnm.api.ApiConnector;
+import org.mnm.api.TokenSupplier;
+import org.mnm.config.Messages;
 
 public class CliTwoFactorTokenSupplier implements TokenSupplier {
 
@@ -27,18 +26,20 @@ public class CliTwoFactorTokenSupplier implements TokenSupplier {
         if ("email".contains(method)) {
             logger.info("Authentication method: {}", method);
             System.out.println(Messages.TwoFactorAuthentication.EMAIL_INSTRUCTIONS);
+            System.out.print("Code: ");
+            String line = readLine();
+            String token = apiConnector.twoFactorAuthentication(method, line, challengeToken);
+            System.out.println("Successfully authenticated");
+            return token;
+        } else {
+            throw new RuntimeException("Unsupported authentication method: " + method);
         }
-        String line = readLine();
-
-        System.out.println(line);
-
-        return line;
     }
 
 
     private String readLine() {
         Scanner s = new Scanner(System.in);
-        String line = s.nextLine();
+        String line = s.nextLine().trim();
 
         s.close();
         return line;
