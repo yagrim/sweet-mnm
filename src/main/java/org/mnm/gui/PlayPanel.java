87658182ae13b523ae4db1b3fa452a5aa8081fd4
yagrim <280896461+yagrim@users.noreply.github.com>
2026-09-24 00:53:18 +0200

    WIP

--- src/main/java/org/mnm/gui/PlayPanel.java
+++ src/main/java/org/mnm/gui/PlayPanel.java
@@ -1,8 +1,13 @@
 package org.mnm.gui;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.util.function.Supplier;
 
@@ -10,6 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.mnm.client.RunnerOptions;
+import org.mnm.config.Environment;
+import org.mnm.config.VersionDetails;
 import org.mnm.events.ClientEventHandler;
 import org.mnm.events.LoginListener;
 import org.mnm.events.Refreshable;
@@ -20,6 +27,7 @@ import static org.mnm.config.Client.Status.UPDATED;
 import static org.mnm.gui.GuiComponents.setFontSize;
 import static org.mnm.gui.MessageDialog.showErrorMessageDialogSync;
 import static org.mnm.gui.Style.ACTION_BUTTON_FONT_SIZE;
+import static org.mnm.gui.Style.BASE_FONT_SIZE;
 import static org.mnm.gui.Style.SCALE;
 
 public class PlayPanel extends JPanel
@@ -40,10 +48,12 @@ public class PlayPanel extends JPanel
         Supplier<RunnerOptions> optionsSupplier,
         float uiScaling) {
 
-        super(new FlowLayout(FlowLayout.CENTER, 0, 0));
-        setBorder(BorderFactory.createEmptyBorder(2 * SCALE, 0, 2 * SCALE, 0));
+//        super(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(2 * SCALE, 0, 0, 0));
 
         play = createButton("Play", uiScaling);
+        play.setAlignmentX(Component.CENTER_ALIGNMENT);
         play.addActionListener(_ -> {
             try {
                 runAction.run(optionsSupplier.get());
@@ -53,6 +63,19 @@ public class PlayPanel extends JPanel
             }
         });
         this.add(play);
+
+        JPanel versionPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        versionPanel.setBorder(BorderFactory.createEmptyBorder(0, SCALE, SCALE, SCALE));
+
+        VersionDetails version = Environment.versionDetails();
+        String text = "Sweet v%s (%s)".formatted(version.version(), version.gitSha());
+
+        JLabel versionLabel = new JLabel(text);
+        versionLabel.setForeground(Color.GRAY);
+        GuiComponents.setFontSize(versionLabel, (float) (BASE_FONT_SIZE * 0.9 * uiScaling));
+        versionPanel.add(versionLabel);
+
+        this.add(versionPanel);
 
         ClientEventHandler.getInstance().register(this);
     }
